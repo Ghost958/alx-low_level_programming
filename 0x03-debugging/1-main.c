e58e5882cffd7d53def652b8ef8b4dad0ae55dfb
Ghost958 <102538000+Ghost958@users.noreply.github.com>
2022-06-12 11:05:55 +0300

    LCS

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,7 +13,12 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-/*while (i < 10){putchar(i);}*/
+/**
+ * while (i < 10)
+ * {
+ *putchar(1);
+ * }
+ */
 
 printf("Infinite loop avoided! \\o/\n");
 
